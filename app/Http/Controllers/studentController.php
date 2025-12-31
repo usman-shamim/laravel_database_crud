bf99af2c5419a05ba1013a95333ef8f3fad9cebf
usman-shamim <usman2007.ap@gmail.com>
2025-12-31 20:04:06 +0500

    completed code in studentController

--- app/Http/Controllers/studentController.php
+++ app/Http/Controllers/studentController.php
@@ -2,9 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Container\Attributes\DB;
 use Illuminate\Http\Request;
 
 class studentController extends Controller
 {
-    //
+    public function index()
+    {
+        $students = DB::table('students')->get();
+        return view('students.index', compact('students'));
+    }
+
+    public function store(Request $request)
+    {
+        DB::table('students')->insert([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone
+        ]);
+        return redirect('/students')->with('success','Student Added Successfully');
+    }
+
+    public function edit($id){
+        $student = DB::table('students')->where('id',$id)->first();
+        return view('students.edits', compact('student'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        DB::table('students')
+            ->where('id', $id)
+            ->update([
+                'name' => $request->name,
+                'email' => $request->email,
+                'phone' => $request->phone
+            ]);
+
+        return redirect('/students')->with('success', 'Student Update Successfully');
+    }
+
+    public function destroy($id){
+        DB::table('students')->where('id',$id)->delete();
+
+        return redirect('/students')->with('Success', 'Students Deleted Successfully');
+    }
 }
