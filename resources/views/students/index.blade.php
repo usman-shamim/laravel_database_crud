23604ae587e3b803ec3dbc6e142ac99d33bdcc85
usman-shamim <usman2007.ap@gmail.com>
2025-12-31 20:10:22 +0500

    did some stylling using bootstrap

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -6,11 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Students CRUD</title>
 </head>
-<body>
+<body class="container mt-5 bg-dark text-white p-4 rounded">
     
     <h2>Add Students</h2>
 
-    <form action="/students/store">
+    <form action="/students/store" method="POST">
         @csrf
         <input type="text" name="name" placeholder="Name" required>
         <input type="text" name="email" placeholder="Email" required>
@@ -26,12 +26,12 @@
         <p style="color: green;">{{session('success')}}</p>
     @endif
     
-    <table>
-        <tr>
+    <table border="1" cellpadding="10" class="table table-bordered table-striped text-white">
+        <tr class="table-light table-bordered">
             <th>ID</th>
             <th>Name</th>
             <th>Email</th>
-            <th>Phone</th>
+            <th>Contact</th>
             <th>Action</th>
         </tr>
 
@@ -40,10 +40,11 @@
             <td>{{ $student->id}}</td>
             <td>{{ $student->name}}</td>
             <td>{{ $student->email}}</td>
-            <td>{{ $student->phone}}</td>
+            <td>{{ $student->contact}}</td>
             <td>
-                <a href="/students/edit/{{$student->id}}">Edit</a>
+                <a href="/students/edit/{{$student->id}}" class="btn btn-warning">Edit</a>
                 <a href="/students/delete/{{$student->id}}"
+                    class="btn btn-danger"
                     onclick="return confirm('Are you sure?')">
                     Delete</a>
             </td>
